47d22314d02362bb6fa7d1baf83d6e2add287e47
gterra <gterra.eca@gmail.com>
2020-05-16 14:43:32 -0300

    Updates

---
+++ precache-manifest.156ef8bc7fb26a5569efcce2ffd2352a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c3106d7a66df02bd58182b636bd38d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8e729db9becf7102de92",
+    "url": "/monsters-rolodex/static/css/main.2a221291.chunk.css"
+  },
+  {
+    "revision": "2126805bdc385eae7cd9",
+    "url": "/monsters-rolodex/static/js/2.0f1a2c81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0f1a2c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e729db9becf7102de92",
+    "url": "/monsters-rolodex/static/js/main.c3fca9b7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
